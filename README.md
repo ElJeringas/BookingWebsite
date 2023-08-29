7a1bac32957e4804f229a1e76be25e3aed7deb0b
ElJeringas <santiagojss32@gmail.com>
2023-08-29 15:28:13 -0500

    adding readme and navbar styling

--- README.md
+++ README.md
@@ -1,8 +1,75 @@
-# React + Vite
+# Hotel Booking Website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Hotel Booking Website Readme
 
-Currently, two official plugins are available:
+Welcome to the README file for our Hotel Booking website! This repository contains a web application built using JavaScript, HTML, CSS, React.js, and the Material-UI (MUI) framework.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+Our Hotel Booking website is a modern web application that enables users to search for hotels, view available rooms, and make reservations. It offers an intuitive user interface and takes advantage of React.js and Material-UI to deliver a responsive and enjoyable experience.
+
+## Features
+
+- User Registration and Authentication
+- Hotel Search and Advanced Filtering
+- Detailed Hotel Information, including Room Options
+- Reservation Management
+- User Dashboard for Easy Reservation Tracking
+- Responsive Design for Mobile and Desktop
+
+## Getting Started
+
+Follow these steps to get the project up and running on your local machine.
+
+### Prerequisites
+
+- Node.js and npm (Node Package Manager) must be installed on your machine.
+
+### Installation
+```bash
+   git clone https://github.com/your-username/hotel-booking-website.git
+```
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/hotel-booking-website.git
+    ```
+2. Navigate to the project directory:
+   ```bash
+    cd hotel-booking-website
+    ```
+3. Install the dependencies:
+  ```bash
+   npm install
+   ```
+### Usage
+1. Start the development server:
+     ```bash
+   npm start
+   ```
+2.  Open your web browser and visit http://localhost:3000 to access the Hotel Booking website.
+3.  Explore the website, search for hotels, view details, and make reservations.
+### Contributing
+Contributions are welcome! If you'd like to contribute to this project, follow these steps:
+
+1.  Fork the repository.
+2.  Create a new branch for your feature or bug fix:
+    ```bash
+     git checkout -b feature-name
+     ```
+3.  Make your changes and commit them with descriptive commit messages.
+4.  Push your changes to your forked repository.
+5.  Create a pull request detailing your changes.
+### License
+This project is licensed under the MIT License.
--- src/pages/HotelBar.jsx
+++ src/pages/HotelBar.jsx
@@ -1,22 +1,95 @@
 import AppBar from '@mui/material/AppBar';
-import { Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Button, IconButton, Toolbar, Typography,Box, Divider,Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/style.css'
 import { green } from '@mui/material/colors';
-function HotelBar() {
+import { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Rooms','Gallery'];
+
+function HotelBar(props) {
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{textAlign:'center'}} >
+      <Typography variant='h6' sx={{my:2}}>
+        Casa Bolivar
+      </Typography>
+      <Divider/>
+{/* 
+          {navItems.map((item)=>(
+            <div className='navlinks' key={item}>
+        <ul id='menulist'>
+            <il key={item}>
+            <a href='# ${`item`}'>{item}</a>
+            </il>
+            </ul>
+      </div>
+          ))} */}
+
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" position="relative" sx={{background:'white'}}>
+        <Toolbar>
+{/*           <IconButton
+            color="black"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>*/}
+          <Typography variant='h7' component='div' sx={{flexGrow:1, color:'black', fontFamily:['Poppins', 'Raleway', 'sans-serif',]}}
+          >
+            Casa Bolivar
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{flexGrow:1, color:'black', fontFamily:['Poppins', 'Raleway', 'sans-serif',]}}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+/*   return (
   
     <AppBar position="relative" sx={{background:'white'}}>
     <Toolbar>
-{/*       <IconButton
-        size='large'
-        edge='start'
-        color='black'
-        aria-label='menu'
-        sx={{mr:2}}
-      >
-        <MenuIcon/>
-      </IconButton> */}
       <Typography variant='h7' component='div' sx={{flexGrow:1, color:'black', fontFamily:['Poppins', 'Raleway', 'sans-serif',]}}>
         Casa Bolivar
       </Typography>
@@ -41,10 +114,11 @@ function HotelBar() {
       </div>
 
     </Toolbar>
-    </AppBar>
+    </AppBar> 
 
 
   )
+  */
 }
 
 export default HotelBar
