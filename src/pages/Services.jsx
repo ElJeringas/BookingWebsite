d9a6ed757832829aeaa9e9fcc93fa6e46a34eaa4
ElJeringas <santiagojss32@gmail.com>
2023-09-19 13:53:50 -0500

    stylying

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -12,22 +12,22 @@ function Services() {
   return (
     <section className='counter top'>
       <div className='container grid'>
-        <div className='box'>
+        <div className='services-box'>
           <h1>{t(`services.cafe`)}</h1>
           <img className='icon' src={coffee} alt='' width='58%' height='58%'/>
           <hr/>
         </div>
-        <div className='box'>
+        <div className='services-box'>
           <h1>{t(`services.hotel`)}</h1>
           <img className='icon' src={hotel} alt='' width='50%' height='50%'/>
           <hr/>
         </div>
-        <div className='box'>
+        <div className='services-box'>
           <h1>{t(`services.photo`)}</h1>
           <img className='icon' src={photo} alt='' width='40%' height='40%'/>
           <hr/>
         </div>
-        <div className='box'>
+        <div className='services-box'>
           <h1>{t(`services.rings`)}</h1>
           <img className='icon' src={wedding} alt='' width='50%' height='50%'/>
           <hr/>
